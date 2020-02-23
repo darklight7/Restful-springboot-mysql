30ff4f6d75fbb351446907eec61fa0174f8f781f
darklight7 <siddhant.nagelia@gmail.com>
2020-02-23 23:16:28 +0530

    added apache

--- src/main/java/com/example/Demo/DemoApplication.java
+++ src/main/java/com/example/Demo/DemoApplication.java
@@ -3,13 +3,22 @@ package com.example.Demo;
 import com.example.Demo.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+		return application.sources(DemoApplication.class);
+}
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
